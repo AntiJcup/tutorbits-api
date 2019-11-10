855174a15d79229c4262457422e51db546fffb8f
AntiJcup <jacobkyle99@yahoo.com>
2019-11-10 10:07:12 -0800

    Add aws lambda project

---
+++ libs/lambda/AWSLambda/AWSLambdaLayerInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TutorBits.LambdaAccess;
+using Utils.Common;
+
+namespace TutorBits.Lambda.AWSLambda
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddAWSLambdaAccessLayer(this IServiceCollection services)
+        {
+            services.AddTransient<LambdaLayerInterface, AWSLambdaLayerInterface>();
+            return services.AddTransient<LambdaAccessService>();
+        }
+    }
+
+    public class AWSLambdaLayerInterface : LambdaLayerInterface
+    {
+        public async Task ConvertWebmToMp4(string webmPath, string outMp4Path)
+        {
+            // TODO Run this lambda
+        }
+    }
+}
