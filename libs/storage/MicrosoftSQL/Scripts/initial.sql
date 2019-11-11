db131fc0ce6632a668c4973f5af35c053a8e6996
AntiJcup <jacobkyle99@yahoo.com>
2019-11-11 08:56:07 -0800

    Add ability to generate migration scripts

---
+++ libs/storage/MicrosoftSQL/Scripts/initial.sql
@@ -0,0 +1,32 @@
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Tutorials] (
+    [Id] uniqueidentifier NOT NULL,
+    [DateCreated] datetime2 NOT NULL DEFAULT (GETUTCDATE()),
+    [DateModified] datetime2 NOT NULL DEFAULT (GETUTCDATE()),
+    [Notes] nvarchar(1028) NULL,
+    [Status] nvarchar(64) NOT NULL,
+    [UserId] uniqueidentifier NULL,
+    [Title] nvarchar(64) NULL,
+    [Language] nvarchar(64) NULL,
+    [Description] nvarchar(1028) NULL,
+    CONSTRAINT [PK_Tutorials] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20191108170414_Initial', N'2.2.6-servicing-10079');
+
+GO
+
+
