11f085cf9de13a4191916d09a83ea926a20f13d3
AntiJcup <jacobkyle99@yahoo.com>
2019-12-01 10:37:23 -0800

    Break out name validation

--- libs/service/AccountAccess/AccountAccessService.cs
+++ libs/service/AccountAccess/AccountAccessService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using TutorBits.AuthAccess;
@@ -52,6 +53,18 @@ namespace TutorBits.AccountAccess
             return await dbService_.CreateBaseModel(account);
         }
 
+        public bool ValidateNickName(string nickName)
+        {
+            var regex = new Regex("^[a-z0-9._-]+$", RegexOptions.IgnoreCase);
+            return regex.IsMatch(nickName);
+        }
+
+        public async Task<bool> DoesNickNameExist(string nickName)
+        {
+            return (await dbService_.GetAllBaseModel((Expression<Func<Account, Boolean>>)(m => m.NickName == nickName && m.Status != BaseState.Deleted)))
+                .Any();
+        }
+
         public async Task<Account> UpdateNickName(Account account, string nickName)
         {
             if (account == null)
@@ -59,8 +72,13 @@ namespace TutorBits.AccountAccess
                 throw new Exception("Account not found");
             }
 
+            if (!ValidateNickName(nickName))
+            {
+                throw new Exception("Invalid name");
+            }
+
             account.NickName = nickName;
-            if ((await dbService_.GetAllBaseModel((Expression<Func<Account, Boolean>>)(m => m.NickName == nickName && m.Status != BaseState.Deleted))).Any())
+            if ((await DoesNickNameExist(nickName)))
             {
                 throw new Exception("Account name already taken");
             }
