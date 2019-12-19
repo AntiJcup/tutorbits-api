56becc1c214d168b886b99e336d4ff9a70328b48
AntiJcup <jacobkyle99@yahoo.com>
2019-12-19 08:42:05 -0800

    Update normalize js lambda to add threads 0 for better performance

--- libs/lambda/functions/AudioNormalizeJS/src/index.js
+++ libs/lambda/functions/AudioNormalizeJS/src/index.js
@@ -11,15 +11,13 @@ exports.handler = function (eventObject, context) {
 		workdir = os.tmpdir(),
 		outputFile = eventObject.Mp4Path,
 		localInputFile = path.join(workdir, "local.webm"),
-		localOutputFile = path.join(workdir, "local.mp4"),
-		localNormalOutputFile = path.join(workdir, "localN.mp4"),
-		localQuietOutputFile = path.join(workdir, "localQ.mp4");
+		localOutputFile = path.join(workdir, "local.mp4");
 
 	console.log('converting', inputBucket, 'using', inputFile);
 	return s3Util.downloadFileFromS3(inputBucket, inputFile, localInputFile)
 		.then(() => childProcessPromise.spawn(
 			'/opt/bin/ffmpeg',
-			['-loglevel', 'error', '-y', '-i', localInputFile, '-vcodec', 'libx264', '-vprofile', 'high', '-preset', 'ultrafast', '-b:v', '500k', '-maxrate', '500k', '-bufsize', '3000m', '-vf', 'scale=-1:480', '-codec:a', 'aac', '-strict', 'experimental', '-b:a', '128k', '-af', 'highpass=200, lowpass=1500, loudnorm=I=-35:TP=-1.5:LRA=20', localOutputFile],
+			['-loglevel', 'error', '-y', '-i', localInputFile, '-vcodec', 'libx264', '-vprofile', 'high', '-preset', 'ultrafast', '-b:v', '500k', '-maxrate', '500k', '-bufsize', '3000m', '-vf', 'scale=-1:480', '-codec:a', 'aac', '-strict', 'experimental', '-b:a', '128k', '-af', 'highpass=200, lowpass=1500, loudnorm=I=-35:TP=-1.5:LRA=20', '-threads', '0', localOutputFile],
 			{ env: process.env, cwd: workdir }
 		).then(() => s3Util.uploadFileToS3(inputBucket, outputFile, localOutputFile, MIME_TYPE)));
 };
