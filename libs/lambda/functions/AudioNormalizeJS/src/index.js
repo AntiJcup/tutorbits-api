30962daa072fb358c44b8b2746f5c37324e113c9
AntiJcup <jacobkyle99@yahoo.com>
2019-12-21 08:16:51 -0800

    Speed up audionormalize

--- libs/lambda/functions/AudioNormalizeJS/src/index.js
+++ libs/lambda/functions/AudioNormalizeJS/src/index.js
@@ -17,7 +17,7 @@ exports.handler = function (eventObject, context) {
 	return s3Util.downloadFileFromS3(inputBucket, inputFile, localInputFile)
 		.then(() => childProcessPromise.spawn(
 			'/opt/bin/ffmpeg',
-			['-loglevel', 'error', '-y', '-i', localInputFile, '-vcodec', 'libx264', '-filter:v', 'scale=-1:920', '-preset', 'medium', '-codec:a', 'aac', '-strict', 'experimental', '-af', 'highpass=200, lowpass=1500, loudnorm=I=-35:TP=-1.5:LRA=20', '-threads', '0', localOutputFile],
+			['-loglevel', 'error', '-y', '-i', localInputFile, '-vcodec', 'libx264', '-filter:v', 'scale=-1:720', '-preset', 'veryfast', '-codec:a', 'aac', '-strict', 'experimental', '-af', 'highpass=200, lowpass=1500, loudnorm=I=-35:TP=-1.5:LRA=20', '-threads', '0', localOutputFile],
 			{ env: process.env, cwd: workdir }
 		).then(() => s3Util.uploadFileToS3(inputBucket, outputFile, localOutputFile, MIME_TYPE)));
 };
